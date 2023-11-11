26da3ca1cfb172480feaeb7148f20fa759c1f5f3
Aleksey Omelchenko <omel.alex123@yandex.ru>
2023-11-11 16:10:25 +0300

    Убрали плагин i18next

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import { createRoot } from 'react-dom/client';
 import App from './app/App';
 
-import './shared/config/i18n/i18n';
-
 const container = document.getElementById('root');
 const root = createRoot(container);
 root.render(
